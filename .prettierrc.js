583fad7e7da4baee93bc3b278b692aae7aef18ad
Takuya Hirata <takuyahirata4@gmail.com>
2022-11-27 11:35:27 -0500

    Chore: init with prettier settings

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  singleQuote: true,
+  tabWith: 2,
+  semi: false,
+  trailingCommna: "none"
+};
---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React, {type PropsWithChildren} from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+export default function App () {
+  return (
+    <SafeAreaView>
+    <Text>here</Text>
+    </SafeAreaView>
+  );
+};
+
